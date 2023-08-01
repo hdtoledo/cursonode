b5046935350fbc063de82032dc32690a8e5d0de1
hdtoledo <hdtoledo@gmail.com>
2023-08-01 15:37:07 -0500

    Cambios pack json

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ app.set('view engine', 'ejs')
 app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 console.log("Servidor activo en el puerto: ", 3000)
